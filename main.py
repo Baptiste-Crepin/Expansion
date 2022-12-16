ddc4b6224134f4fee22b5898b21982ec53b24075
Baptiste-Crepin <81614462+Nypec@users.noreply.github.com>
2022-12-16 22:19:51 +0100

    added player and size verif
    added verification for the size of the grid and a first iteration of the player

--- main.py
+++ main.py
@@ -1,39 +1,108 @@
 class Case():
 
-    def __init__(self, pawnNumber: int, player: int):
-        self.pawnNumber = pawnNumber
-        self.player = player
+    def __init__(self, pawnNumber: int, player: int) -> None:
+        self.__pawnNumber = pawnNumber
+        self.__player = player
 
-    def getPawnNumber(self):
-        return self.pawnNumber
+    def getPawnNumber(self) -> int:
+        return self.__pawnNumber
 
-    def getPlayer(self):
-        return self.player
+    def getPlayer(self) -> int:
+        return self.__player
 
-    def setPawnNumber(self, value: int):
-        self.pawnNumber = value
+    def setPawnNumber(self, value: int) -> None:
+        self.__pawnNumber = value
 
-    def setPlayer(self, value: int):
-        self.player = value
+    def setPlayer(self, value: int) -> None:
+        self.__player = value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str((self.getPawnNumber(), self.getPlayer()))
 
 
 class Jeu():
-    def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
-        self.grid = [[Case(0, 0) for x in range(self.width)]
-                     for y in range(self.height)]
+    def __init__(self, width: int, height: int, NumberOfPlayers) -> None:
+        self.__width = self.validWidth(width)
+        self.__height = self.validHeight(height)
+        self.__NumberOfPlayers = self.validNumberOfPlayers(NumberOfPlayers)
+        print(self.__height)
+        self.__grid = self.createGrid()
 
-    def display(self):
-        for row in self.grid:
+        while not self.engoughSpaceForPlayer():
+            self.expandBoard(self.getWidth()+1, self.getHeight()+1)
+
+    def getWidth(self) -> int:
+        return self.__width
+
+    def getHeight(self) -> int:
+        return self.__height
+
+    def getNumberOfPlayers(self) -> int:
+        return self.__NumberOfPlayers
+
+    def getGrid(self) -> list:
+        return self.__grid
+
+    def setWidth(self, value: int) -> None:
+        self.__width = value
+
+    def setHeight(self, value: int) -> None:
+        self.__height = value
+
+    def setNumberOfPlayers(self, value: int) -> None:
+        self.__NumberOfPlayers = value
+
+    def setGrid(self, value: list) -> None:
+        self.__grid = value
+
+    def createGrid(self) -> list:
+        return [[Case(0, 0) for x in range(self.getWidth())]
+                for y in range(self.getHeight())]
+
+    def validWidth(self, width) -> None:
+        if width < 3:
+            self.setWidth(3)
+            return self.getWidth()
+        if width > 12:
+            self.setWidth(12)
+            return self.getWidth()
+
+    def validHeight(self, height) -> None:
+        if height < 3:
+            self.setHeight(3)
+            return self.getHeight()
+        if height > 10:
+            self.setHeight(10)
+            return self.getHeight()
+
+    def validNumberOfPlayers(self, NumberOfPlayer) -> None:
+        if NumberOfPlayer < 2:
+            self.setNumberOfPlayers(2)
+            return self.getNumberOfPlayers()
+        if NumberOfPlayer > 8:
+            self.setNumberOfPlayers(8)
+            return self.getNumberOfPlayers()
+
+    def display(self) -> None:
+        for row in self.getGrid():
             print(" | ".join(map(str, row)))
+
+    def expandBoard(self, width: int, height: int):
+        self.setWidth(width)
+        self.setHeight(height)
+        self.setGrid(self.createGrid())
+
+    def engoughSpaceForPlayer(self) -> bool:
+        if self.getWidth() * self.getHeight() >= self.getNumberOfPlayers():
+            return True
+        return False
 
 
 if __name__ == "__main__":
     # import doctest
     # doctest.testmod(verbose=True)
-    J1 = Jeu(3, 4)
+    J1 = Jeu(1, 1, 9999)
     J1.display()
+    print(J1.engoughSpaceForPlayer())
+    J1.display()
+    print(J1.getNumberOfPlayers())
