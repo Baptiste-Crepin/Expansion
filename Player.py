6abdbe9bb6b1198043d0e83a0cb1c63c8647768a
Baptiste-Crepin <81614462+Nypec@users.noreply.github.com>
2022-12-17 02:43:33 +0100

    Working prototype
    created the game loop
    
    added checkWin function
    added color in Player class

--- Player.py
+++ Player.py
@@ -1,7 +1,7 @@
 class Player():
-    def __init__(self, number: int, color: str):
+    def __init__(self, number: int):
         self.__number = number
-        self.__color = color
+        self.__color = self.setColorFromNumber()
 
     def getNumber(self) -> int:
         return self.__number
@@ -12,8 +12,26 @@ class Player():
     def setNumber(self, value: int) -> None:
         self.__number = value
 
-    def setNumber(self, value: str) -> None:
+    def setColor(self, value: str) -> None:
         self.__color = value
 
     def __repr__(self) -> str:
+        print(self.getColor())
         return self.getColor()
+
+    def setColorFromNumber(self) -> None:
+        BLACK = "#FFFFFF"
+        RED = "#c3463d"
+        ORANGE = "#d26f34"
+        YELLOW = "#efe743"
+        GREEN = "#639d39"
+        BLUE = "#1a6baa"
+        PURPLE = "#6d3757"
+        PINK = "#cc4a8a"
+        TEAL = "#659bfc"
+        colorList = [BLACK, RED, ORANGE,
+                     YELLOW, GREEN,
+                     BLUE, PURPLE,
+                     PINK, TEAL]
+
+        return colorList[self.getNumber()]
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Jeu():
         self.__width = self.validWidth(width)
         self.__height = self.validHeight(height)
         self.__NumberOfPlayers = self.validNumberOfPlayers(NumberOfPlayers)
+        self.__PlayerList = self.createPlayerList()
         self.__grid = self.createGrid()
 
         while not self.engoughSpaceForPlayer():
@@ -33,11 +34,14 @@ class Jeu():
     def getHeight(self) -> int:
         return self.__height
 
+    def getGrid(self) -> list:
+        return self.__grid
+
     def getNumberOfPlayers(self) -> int:
         return self.__NumberOfPlayers
 
-    def getGrid(self) -> list:
-        return self.__grid
+    def getPlayerList(self) -> int:
+        return self.__PlayerList
 
     def setWidth(self, value: int) -> None:
         self.__width = value
@@ -45,15 +49,22 @@ class Jeu():
     def setHeight(self, value: int) -> None:
         self.__height = value
 
-    def setNumberOfPlayers(self, value: int) -> None:
-        self.__NumberOfPlayers = value
-
     def setGrid(self, value: list) -> None:
         self.__grid = value
 
+    def setNumberOfPlayers(self, value: int) -> None:
+        self.__NumberOfPlayers = value
+
+    def setPlayerList(self, value: list) -> None:
+        self.__PlayerList = value
+
     def createGrid(self) -> list:
-        return [[Case(0, (x, y), Player(0, "#FFFFFF")) for x in range(self.getWidth())]
+        return [[Case(0, (x, y), Player(0)) for x in range(self.getWidth())]
                 for y in range(self.getHeight())]
+
+    def createPlayerList(self) -> list:
+        print("11", [Player(x+1) for x in range(self.getNumberOfPlayers())])
+        return [Player(x+1) for x in range(self.getNumberOfPlayers())]
 
     def display(self) -> None:
         for row in self.getGrid():
@@ -112,35 +123,66 @@ class Jeu():
 
     def expandPawn(self, coord: tuple, player: Player) -> None:
         cell = self.getCell(coord)
-        print(cell.getPawnNumber(), self.numberOfNeighbours(coord))
         if self.numberOfNeighbours(coord) > cell.getPawnNumber():
             return False
 
         cell.setPawnNumber(0)
-        cell.setPlayer(Player(0, "#FFFFFF"))
+        cell.setPlayer(Player(0))
 
         for neighbour in self.getNeighbours(coord):
             neighbour.setPlayer(player)
             neighbour.setPawnNumber(neighbour.getPawnNumber()+1)
             self.expandPawn(neighbour.getCoordinates(), player)
 
+    def playerInGrid(self, player):
+        for row in self.getGrid():
+            for cell in row:
+                #print(cell.getPlayer(), player)
+                if cell.getPlayer().getNumber() == player.getNumber():
+                    return True
 
-if __name__ == "__main__":
-    G1 = Jeu(1, 1, 9999)
-    P1 = Player(1, "#FF0000")
-    P2 = Player(2, "#000000")
+        return False
+
+    def updatePlayers(self) -> None:
+        for player in self.getPlayerList():
+            if not self.playerInGrid(player):
+                print("LOST", player)
+                self.getPlayerList().remove(player)
+
+    def checkWin(self):
+        if len(self.getPlayerList()) == 1:
+            #print(len(self.getPlayerList()), self.getPlayerList())
+            return True
+        return False
+
+
+def play():
+    G1 = Jeu(int(input("width")), int(input("height")),
+             int(input("player Number")))
+    P1 = G1.getPlayerList()[0]
+    P2 = G1.getPlayerList()[1]
     print()
     print("First Display")
     G1.display()
     print()
 
-    G1.placePawn((0, 0), P1)
-    G1.display()
-    print()
-    G1.placePawn((0, 1), P2)
-    G1.display()
-    print()
-    G1.placePawn((0, 0), P1)
-    G1.expandPawn((0, 0), P1)
-    G1.display()
-    print()
+    for player in G1.getPlayerList():
+        coordo = (int(input("col"))-1, int(input("row"))-1)
+        G1.placePawn(coordo, player)
+        G1.expandPawn(coordo, player)
+        G1.display()
+        print()
+
+    while not G1.checkWin():
+        for player in G1.getPlayerList():
+            print(player.getNumber())
+            coordo = (int(input("col"))-1, int(input("row"))-1)
+            G1.placePawn(coordo, player)
+            G1.expandPawn(coordo, player)
+            G1.display()
+            G1.updatePlayers()
+            print()
+
+
+if __name__ == "__main__":
+    play()
